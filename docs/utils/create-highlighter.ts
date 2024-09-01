3e752755ab79be31f0083d544b5ac783b4c43cf8
Azat S. <to@azat.io>
2024-09-01 20:34:37 +0700

    docs: move to js shiki engine

--- docs/utils/create-highlighter.ts
+++ docs/utils/create-highlighter.ts
@@ -1,12 +1,18 @@
 import type { HighlighterGeneric, BundledLanguage, BuiltinTheme } from 'shiki'
 
-import { createHighlighter as createShikiHighlighter } from 'shiki'
+import {
+  createHighlighter as createShikiHighlighter,
+  createJavaScriptRegexEngine,
+} from 'shiki'
 
 let highlighter: HighlighterGeneric<BundledLanguage, BuiltinTheme> | null = null
+
+let jsEngine = createJavaScriptRegexEngine()
 
 export let createHighlighter = async () => {
   if (!highlighter) {
     highlighter = await createShikiHighlighter({
+      engine: jsEngine,
       langs: ['tsx'],
       themes: [],
     })
